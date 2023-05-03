520c869e34459f70028c7720ffe761f59b0ef6c3
vinccodes <vinccodes@gmail.com>
2023-05-02 22:49:19 -0400

    added import ./server for miragejs, fetched vans from endpoint /api/vans via miragejs. Mapped the vans array into JSX elements.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+// miragejs for mock api
+import './server';
 
 
 
--- src/pages/Vans/Vans.js
+++ src/pages/Vans/Vans.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import './Vans.css'
+
 const Vans = ()=>{
+
+    const [vans, setVans] = useState([]);
+
+    useEffect(()=>{
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data=> setVans(data.vans))
+    }, [])
+    
     return (
         <div className="vans">
             <div className="container">
                 <h1 className="home__lead">Explore our van options</h1>
+                <div className="container">
+                {
+                    vans.map(van=>{
+                        return (
+                            <div key={van.id}>
+                                <img src={van.imageUrl} alt="van-image"></img>
+                                <h3>{van.name}</h3>
+                                <p>{van.price}</p>
+                                <p>{van.description}</p>
+                                <p>{van.type}</p>
+                            </div>
+                        )
+                    })
+                }
+                </div>
             </div>
         </div>
     )
